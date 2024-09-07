eff9e5fbac40a17de0f914ef43a7d0f746ef5a93
Aston Motes <astonm@gmail.com>
2024-09-07 17:39:45 -0400

    Restore tests/test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,168 +1,75 @@
 # coding: pyxl_fasthtml
+import pytest
+import re
 import unittest
+
+from fasthtml.common import *
+
 from pyxl_fasthtml import html
 from pyxl_fasthtml.base import PyxlFasthtmlException, x_base
 
+def Frag(*x): return x
+
 class PyxlFasthtmlTests(unittest.TestCase):
+    def assertEqualHTML(self, elm, s):
+        elm_s = re.sub(r"\n\s*", "", to_xml(elm))
+        return self.assertEqual(elm_s, s)
 
     def test_basics(self):
-        self.assertEqual(<div />.to_string(), '<div></div>')
-        self.assertEqual(<img src="blah" />.to_string(), '<img src="blah" />')
-        self.assertEqual(<div class="c"></div>.to_string(), '<div class="c"></div>')
-        self.assertEqual(<div><span></span></div>.to_string(), '<div><span></span></div>')
-        self.assertEqual(<frag><span /><span /></frag>.to_string(), '<span></span><span></span>')
+        self.assertEqualHTML(<div />, '<div></div>')
+        self.assertEqualHTML(<img src="blah" />, '<img src="blah">')
+        self.assertEqualHTML(<div cls="c"></div>, '<div class="c"></div>')
+        self.assertEqualHTML(<div><span></span></div>, '<div><span></span></div>')
+        self.assertEqualHTML(<frag><span /><span /></frag>, '<span></span><span></span>')
 
     def test_escaping(self):
-        self.assertEqual(<div class="&">&{'&'}</div>.to_string(), '<div class="&amp;">&&amp;</div>')
-        self.assertEqual(<div>{html.rawhtml('&')}</div>.to_string(), '<div>&</div>')
+        self.assertEqualHTML(<div cls="&">{'&'}</div>, '<div class="&amp;">&amp;</div>')
+        self.assertEqualHTML(<div>{html.rawhtml('&')}</div>, '<div>&</div>')
 
-    def test_comments(self):
-        pyxl_fasthtml = (
-            <div
-                class="blah" # attr comment
-                >  # comment1
-                <!-- comment2 -->
-                text# comment3
-                # comment4
-            </div>)
-        self.assertEqual(pyxl_fasthtml.to_string(), '<div class="blah">text</div>')
+    def test_multiline(self):
+        div = (
+            <div>
+                text
+            </div>
+        )
+        self.assertEqualHTML(div, '<div>text</div>')
+
+        div2 = (
+            <div attr="attr">
+                text
+            </div>
+        )
+        self.assertEqualHTML(div2, '<div attr="attr">text</div>')
+
+    def test_python_comment(self):
+        with_comment = (
+            <div cls="blah">
+                # comment
+                text
+            </div>
+        )
+        self.assertEqualHTML(with_comment, '<div class="blah">text</div>')
+
+    def test_html_comment(self):
+        with pytest.raises(NameError):
+            comment = <!-- comment -->
 
     def test_cond_comment(self):
-        s = 'blahblah'
-        self.assertEqual(
-            <cond_comment cond="lt IE 8"><div class=">">{s}</div></cond_comment>.to_string(),
-            '<!--[if lt IE 8]><div class="&gt;">blahblah</div><![endif]-->')
-        self.assertEqual(
-            <cond_comment cond="(lt IE 8) & (gt IE 5)"><div>{s}</div></cond_comment>.to_string(),
-            '<!--[if (lt IE 8) & (gt IE 5)]><div>blahblah</div><![endif]-->')
+        with pytest.raises(NameError):
+            cdata = <!--[if lt IE 8]>blahblah<![endif]-->
 
     def test_decl(self):
-        self.assertEqual(
-            <script><![CDATA[<div><div>]]></script>.to_string(),
-            '<script><![CDATA[<div><div>]]></script>')
-
-    def test_form_error(self):
-        self.assertEqual(
-            <form_error name="foo" />.to_string(),
-            '<form:error name="foo" />')
-
+        with pytest.raises(NameError):
+            cdata = <script><![CDATA[<div><div>]]></script>
 
     def test_if(self):
-        self.assertEqual(
-            <div><if cond="{True}"><div /></if></div>.to_string(),
-            '<div><div></div></div>')
-        self.assertEqual(
-            <div><if cond="{False}"><div /></if></div>.to_string(),
-            '<div></div>')
+        with pytest.raises(NameError):
+            if_tag = <if cond="{True}"></if>
 
     def test_if_else(self):
-        lol = [<span></span>, <span></span>]
+        with pytest.raises(NameError):
+            else_tag = <else>lol</else>
 
-        self.assertEqual(
-            <div>
-            <if cond="{True}">
-                <div />
-            </if>
-            # welp
-            lol
-            </div>.to_string(),
-            '<div><div></div>lol</div>')
-        self.assertEqual(
-            <div>
-            <if cond="{True}"></if>
-            # welp
-            <else>lol</else>
-            </div>.to_string(),
-            '<div></div>')
-        self.assertEqual(
-            <div>
-            <if cond="{True}"></if>
-            </div>.to_string(),
-            '<div></div>')
-        self.assertEqual(
-            <div>
-            <if cond="{False}"><div /></if>
-            <if cond="{True}"><div /></if>
-            {lol}
-            </div>.to_string(),
-            '<div><div></div><span></span><span></span></div>')
-        self.assertEqual(
-            <div>
-            <if cond="{True}"><div /></if>
-            <else>test</else>
-            </div>.to_string(),
-            '<div><div></div></div>')
-        self.assertEqual(
-            <div>
-            <if cond="{True}"><div /><div /></if>
-            <else><span></span></else>
-            </div>.to_string(),
-            '<div><div></div><div></div></div>')
-        self.assertEqual(
-            <div>
-            <if cond="{False}"><div /></if>
-            <else><span></span></else>
-            </div>.to_string(),
-            '<div><span></span></div>')
-
-        self.assertEqual(
-            <div>
-            <if cond="{True if True else False}">
-            whatever
-            </if>
-            </div>.to_string(),
-            '<div>whatever</div>')
-
-        self.assertEqual(
-            <div>
-            <if cond="{True}">
-                {lol}
-                <if cond="{False}">
-                whatever
-                </if>
-            </if>
-            </div>.to_string(),
-            '<div><span></span><span></span></div>')
-
-    def test_enum_attrs(self):
-        class x_foo(x_base):
-            __attrs__ = {
-                'value': ['a', 'b'],
-            }
-
-            def _to_list(self, l):
-                pass
-
-        self.assertEqual(<foo />.attr('value'), 'a')
-        self.assertEqual(<foo />.value, 'a')
-        self.assertEqual(<foo value="b" />.attr('value'), 'b')
-        self.assertEqual(<foo value="b" />.value, 'b')
-        with self.assertRaises(PyxlFasthtmlException):
-            <foo value="c" />
-
-        class x_bar(x_base):
-            __attrs__ = {
-                'value': ['a', None, 'b'],
-            }
-
-            def _to_list(self, l):
-                pass
-
-        with self.assertRaises(PyxlFasthtmlException):
-            <bar />.attr('value')
-
-        with self.assertRaises(PyxlFasthtmlException):
-            <bar />.value
-
-        class x_baz(x_base):
-            __attrs__ = {
-                'value': [None, 'a', 'b'],
-            }
-
-            def _to_list(self, l):
-                pass
-
-        self.assertEqual(<baz />.value, None)
 
 if __name__ == '__main__':
     unittest.main()
